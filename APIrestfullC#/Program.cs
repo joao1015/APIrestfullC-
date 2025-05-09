b0c0a70031ebbd1ceb19cde7a4bc50a3caa6b5d8
joao1015 <moreira.joaopaulo1993@gmail.com>
2025-05-08 22:28:15 -0300

    Adicionar arquivos de projeto.

---
+++ APIrestfullC#/Program.cs
@@ -0,0 +1,27 @@
+using APIrestfullC_.Properties.Data; // Importante: precisa incluir o namespace correto
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registro do DbContext usando sua classe AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
